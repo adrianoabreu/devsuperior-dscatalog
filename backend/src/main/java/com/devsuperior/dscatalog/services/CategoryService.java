752adc4633a4ba4758813b9eeda1189456760fb5
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2022-05-25 22:03:00 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -10,6 +10,8 @@ import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,18 +28,10 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<CategoryDTO> findAll(){
-		List<Category> list = repository.findAll();
-
-// Solução com expressão lambda
-		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
-
-// Solução alternativa		
-//		List<CategoryDTO> listDto = new ArrayList<>();
-//		for (Category cat : list) {
-//			listDto.add(new CategoryDTO(cat));
-//		}
-//		return listDto;		
+	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest){
+		Page<Category> list = repository.findAll(pageRequest);
+		return list.map(x -> new CategoryDTO(x));
+//		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 	
 	@Transactional(readOnly = true)
