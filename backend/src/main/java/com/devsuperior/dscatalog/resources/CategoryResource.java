26110cc69dc69c38997c00508cf0e69d1ad61bfc
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2022-06-13 14:59:53 -0300

    Refactor pageable. Refatoração do projeto para utilizar o objeto Pageable no metodo findAll

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -34,14 +35,8 @@ public class CategoryResource {
 	
 	//ResponseEntity encapsula respostas HTTP
 	@GetMapping  //Determina o Endpoint do controlador REST
-	public ResponseEntity<Page<CategoryDTO>> findAll(
-			@RequestParam(value = "page",         defaultValue = "0")    Integer page,
-			@RequestParam(value = "linesPerPage", defaultValue = "12")   Integer linesPerPage,
-			@RequestParam(value = "direction",    defaultValue = "ASC") String direction,			
-			@RequestParam(value = "orderBy",      defaultValue = "name") String orderBy
-		){		
-		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);	
-		Page<CategoryDTO> list = service.findAllPaged(pageRequest);		
+	public ResponseEntity<Page<CategoryDTO>> findAll(Pageable pageable){		
+		Page<CategoryDTO> list = service.findAllPaged(pageable);		
 		return ResponseEntity.ok().body(list);
 	}
 	
--- backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -12,6 +12,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -34,8 +35,8 @@ public class ProductService {
 	private CategoryRepository categoryRepository;
 	
 	@Transactional(readOnly = true)
-	public Page<ProductDTO> findAllPaged(PageRequest pageRequest){
-		Page<Product> list = repository.findAll(pageRequest);
+	public Page<ProductDTO> findAllPaged(Pageable pageable){
+		Page<Product> list = repository.findAll(pageable);
 		return list.map(x -> new ProductDTO(x));
 //		return list.stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());
 	}
