769112c12f357ac2655930075f7980e8ae9a170c
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2022-07-07 21:06:34 -0300

    User CRUD

---
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.devsuperior.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.devsuperior.dscatalog.entities.Role;
+import com.devsuperior.dscatalog.entities.User;
+
+@Repository //Com esta anotação, a injeção de independência desta classe passa a ser gerenciada pelo Spring.
+public interface RoleRepository extends JpaRepository<Role, Long>{
+
+}
