ebdfb32f7c8365b207b10bb3168106f94ab162f7
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2022-09-03 17:33:13 -0300

    Lidando com o famigerado problema N mais 1 consultas

--- backend/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
@@ -19,4 +19,8 @@ public interface ProductRepository extends JpaRepository<Product, Long>{
 			+ "(COALESCE(:categories) IS NULL OR cats IN :categories) AND "
 			+ "(:name = '' OR LOWER(obj.name) LIKE LOWER(CONCAT('%',:name,'%')))")
 	Page<Product> find(List<Category> categories, String name, Pageable pageable);
+	
+	//Resolvendo problema das N+1 consultas
+	@Query("SELECT obj FROM Product obj JOIN FETCH obj.categories WHERE obj IN :products")
+	List<Product> findProductsWithCategories(List<Product> products);
 }
