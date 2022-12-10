60df1c443e470e76961a70d94f2cd23652b55d88
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2022-12-10 11:14:03 -0300

    Criando o componente ProductPrice

--- frontweb/src/components/ProductCard/index.tsx
+++ frontweb/src/components/ProductCard/index.tsx
@@ -1,5 +1,6 @@
 import './styles.css';
 import ProductImg from 'assets/images/product.png';
+import ProductPrice from 'components/ProductPrice';
 
 const ProductCard = () => {
 
@@ -10,7 +11,7 @@ const ProductCard = () => {
             </div>
             <div className="card-bottom-container">
                 <h6>Nome do produto</h6>
-                <p>2345.67</p>
+                <ProductPrice/>
             </div>
         </div>
     );
