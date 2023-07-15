a7e511d7f09e2e4e03c8b29916d48dfe80a7e168
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2023-07-15 14:15:43 -0300

    Check user authenticated - Testando se o usuário está autenticado

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -2,13 +2,13 @@ import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
 import ButtonIcon from 'components/ButtonIcon';
 import { Link } from 'react-router-dom';
 import './styles.css';
-import { getTokenData } from 'util/requests';
+import {  isAuthenticated } from 'util/requests';
 
 const Home = () => {
   return (
     <div className="home-container">
 
-      <h1>{getTokenData()?.user_name}</h1>
+      <h1>{isAuthenticated() ? 'autenticado' : 'Não autenticado'}</h1>
       
       <div className="base-card home-card">
         <div className="home-content-container">
