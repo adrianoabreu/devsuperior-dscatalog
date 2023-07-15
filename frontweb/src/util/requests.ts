be2119c31734433aedbcfc37fa39f3d86e06d089
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2023-07-15 13:34:41 -0300

    Primeiro teste com interceptors do Axios

--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -56,3 +56,27 @@ export const getAuthData = () => {
     const str = localStorage.getItem(tokenKey) ?? "{}";
     return JSON.parse(str) as LoginResponse;
 }
+
+// Add a request interceptor
+axios.interceptors.request.use(function (config) {
+    // Do something before request is sent
+    console.log('INTERCEPTORS ANTES DA REQUISIÇÃO');
+    return config;
+  }, function (error) {
+    // Do something with request error
+    console.log('INTERCEPTORS ERRO NA REQUISIÇÃO');
+    return Promise.reject(error);
+  });
+
+// Add a response interceptor
+axios.interceptors.response.use(function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    console.log('INTERCEPTORS RESPOSTA COM SUCESSO');
+    return response;
+  }, function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    console.log('INTERCEPTORS RESPOSTA COM ERRO');
+    return Promise.reject(error);
+  });
