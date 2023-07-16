3834a3aaaa3b7bc38b602ab23694aa18e5be16ce
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2023-07-16 13:09:05 -0300

    Links de Login e Logout na Navbar

--- frontweb/src/components/Navbar/index.tsx
+++ frontweb/src/components/Navbar/index.tsx
@@ -1,9 +1,42 @@
 import './styles.css';
 import 'bootstrap/js/src/collapse.js';
+import { useEffect, useState } from 'react';
 
 import { Link, NavLink } from 'react-router-dom';
+import history from 'util/history';
+import { TokenData, getTokenData, isAuthenticated, removeAuthData } from 'util/requests';
+
+type AuthData = {
+  authenticated: boolean,
+  tokenData?: TokenData
+}
 
 const Navbar = () => {
+  const [authData, setAuthData] = useState<AuthData>({authenticated: false});
+
+  useEffect(() => {
+    if(isAuthenticated()){
+      setAuthData({
+        authenticated: true,
+        tokenData: getTokenData()
+      });
+    }
+    else {
+      setAuthData({
+        authenticated: false
+      });
+    }
+  }, []);
+
+  const handleLogoutClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+    removeAuthData();
+    setAuthData({
+      authenticated: false,
+    });
+    history.replace('/');
+  }
+
   return (
     <nav className="navbar navbar-expand-md navbar-dark bg-primary main-nav">
       <div className="container-fluid">
@@ -36,6 +69,16 @@ const Navbar = () => {
               <NavLink to="/admin" activeClassName="active">ADMIN</NavLink>
             </li>
           </ul>
+        </div>
+        <div>
+          {authData.authenticated ? (
+            <>
+              <span>{authData.tokenData?.user_name}</span>
+              <a href="#logout" onClick={handleLogoutClick}>LOGOUT</a>
+            </>
+          ) : (
+            <Link to="/admin/auth">LOGIN</Link>
+          )}
         </div>
       </div>
     </nav>
