ccb14bfb9fd9f12e484d7d5000a65f1d6c41e9bc
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2023-07-16 14:31:23 -0300

    Estilizando o login e logout

--- frontweb/src/components/Navbar/index.tsx
+++ frontweb/src/components/Navbar/index.tsx
@@ -70,10 +70,10 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
-        <div>
+        <div className="nav-login-logout">
           {authData.authenticated ? (
             <>
-              <span>{authData.tokenData?.user_name}</span>
+              <span className="nav-username">{authData.tokenData?.user_name}</span>
               <a href="#logout" onClick={handleLogoutClick}>LOGOUT</a>
             </>
           ) : (
