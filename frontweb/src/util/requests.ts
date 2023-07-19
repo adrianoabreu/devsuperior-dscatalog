6284f5345a66ae1e0b68ac888bcc94ab5eaaa34d
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2023-07-19 00:04:55 -0300

    Implementação alternativa com high order functions

--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -129,6 +129,7 @@ export const hasAnyRoles = (roles: Role[]) : boolean => {
           return true;
         }
       }
+      //return roles.some(role => tokenData.authorities.includes(role));
     }
 
     return false;
