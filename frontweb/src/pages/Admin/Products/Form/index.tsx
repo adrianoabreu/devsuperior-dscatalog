0591a30c91adcb498dee280ef03f9f945278a502
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2023-07-23 12:26:37 -0300

    Estilizando margens e containers do formulário

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -1,30 +1,39 @@
 import './styles.css';
 
 const Form = () => {
-    return (
-        <div className="product-crud-container">
-            <div className="base-card product-crud-form-card">
-                <h1 className="product-crud-form-title">DADOS DO PRODUTO</h1>
-                <form action="">
-                    <div className="row">
-                        <div className="col-lg-6">
-                            <input type="text" className="form-control base-input"/>
-                            <input type="text" className="form-control base-input"/>
-                            <input type="text" className="form-control base-input"/>
-                        </div>
-                        <div className="col-lg-6">
-                            <textarea name="" rows={10} className="form-control base-input"></textarea>
-                        </div>
-                    </div>
-                    <div>
-                        <button className="btn btn-outline-danger">CANCELAR</button>
-                        <button className="btn btn-primary">SALVAR</button>
-                    </div>
-                </form>
+  return (
+    <div className="product-crud-container">
+      <div className="base-card product-crud-form-card">
+        <h1 className="product-crud-form-title">DADOS DO PRODUTO</h1>
+        <form action="">
+          <div className="row product-crud-inputs-container">
+            <div className="col-lg-6 product-crud-inputs-left-container">
+              <div className="margin-bottom-30">
+                <input type="text" className="form-control base-input" />
+              </div>
+              <div className="margin-bottom-30">
+                <input type="text" className="form-control base-input" />
+              </div>
+              <div>
+                <input type="text" className="form-control base-input" />
+              </div>
             </div>
-            
-        </div>
-    )
-}
+            <div className="col-lg-6">
+              <textarea
+                name=""
+                rows={10}
+                className="form-control base-input"
+              ></textarea>
+            </div>
+          </div>
+          <div className="product-crud-buttons-container">
+            <button className="btn btn-outline-danger">CANCELAR</button>
+            <button className="btn btn-primary">SALVAR</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Form;
