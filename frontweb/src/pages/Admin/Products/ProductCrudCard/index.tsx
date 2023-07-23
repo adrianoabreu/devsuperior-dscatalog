f197681a0d40bbbb3f58cef3f139213644173363
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2023-07-23 08:15:20 -0300

    Responsividade do ProductCrudCard na forma horizontal

--- frontweb/src/pages/Admin/Products/ProductCrudCard/index.tsx
+++ frontweb/src/pages/Admin/Products/ProductCrudCard/index.tsx
@@ -14,7 +14,7 @@ const ProductCrudCard = ({product} : Props) => {
             <div className="product-crud-card-top-container">
                 <img src={product.imgUrl} alt={product.name}/>
             </div>
-            <div>
+            <div className="product-crud-card-description">
               <div className="product-crud-card-bottom-container">
                 <h6>{product.name}</h6>
                 <ProductPrice price={product.price}/>
@@ -24,15 +24,15 @@ const ProductCrudCard = ({product} : Props) => {
                     <CategoryBadge name={category.name} key={category.id}/>
                 ))}
               </div>
-              <div className="product-crud-card-buttons-container">
-                <button className="btn btn-outline-danger product-crud-card-button product-crud-card-button-first">
-                    EXCLUIR
-                </button>
-                <button className="btn btn-outline-secondary product-crud-card-button">
-                    EDITAR
-                </button>
-              </div>
-            </div>        
+            </div>
+            <div className="product-crud-card-buttons-container">
+              <button className="btn btn-outline-danger product-crud-card-button product-crud-card-button-first">
+                EXCLUIR
+              </button>
+              <button className="btn btn-outline-secondary product-crud-card-button">
+                EDITAR
+              </button>
+            </div>           
         </div>
     );
 }
