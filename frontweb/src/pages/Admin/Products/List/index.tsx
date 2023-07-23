d994179792a41ff90a58bf2073b6aa5abb48791d
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2023-07-23 14:04:31 -0300

    Listando os produtos a partir do backend

--- frontweb/src/pages/Admin/Products/List/index.tsx
+++ frontweb/src/pages/Admin/Products/List/index.tsx
@@ -1,53 +1,49 @@
-import ProductCrudCard from "pages/Admin/Products/ProductCrudCard";
+import ProductCrudCard from 'pages/Admin/Products/ProductCrudCard';
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { SpringPage } from 'types/vendor/spring';
+import { Product } from 'types/product';
+import { AxiosRequestConfig } from 'axios';
 import './styles.css';
-import { Link } from "react-router-dom";
+import { requestBackend } from 'util/requests';
 
 const List = () => {
+  const [page, setPage] = useState<SpringPage<Product>>();
 
-    const product = {
-        "id": 1,
-        "name": "The Lord of the Rings",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        "price": 90.5,
-        "imgUrl": "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg",
-        "date": "2020-07-13T20:50:07.123450Z",
-        "categories": [
-            {
-                "id": 2,
-                "name": "Eletrônicos"
-            },
-            {
-              "id": 1,
-              "name": "Computadores"
-            },
-            {
-              "id": 3,
-              "name": "Celulares"
-            }
-        ]
-    }
+  useEffect(() => {
+    const config: AxiosRequestConfig = {
+      method: 'GET',
+      url: '/products',
+      params: {
+        page: 0,
+        size: 50,
+      },
+    };
 
-    return (
-      <div className="product-crud-container">
-        <div className="product-crud-bar-container">
-          <Link to="/admin/products/create">
-            <button className="btn btn-primary text-white btn-crud-add">ADICIONAR</button>
-          </Link>  
-          <div className="base-card product-filter-container">Search bar</div>
-        </div>
-        <div className="row">
-            <div className="col-sm-6 col-md-12">
-              <ProductCrudCard product={product} />
-            </div>
-            <div className="col-sm-6 col-md-12">
-              <ProductCrudCard product={product} />
-            </div>
-            <div className="col-sm-6 col-md-12">
-              <ProductCrudCard product={product} />
-            </div>
-        </div>
+    requestBackend(config).then((response) => {
+      setPage(response.data);
+    });
+  }, []);
+
+  return (
+    <div className="product-crud-container">
+      <div className="product-crud-bar-container">
+        <Link to="/admin/products/create">
+          <button className="btn btn-primary text-white btn-crud-add">
+            ADICIONAR
+          </button>
+        </Link>
+        <div className="base-card product-filter-container">Search bar</div>
       </div>
-    );
+      <div className="row">
+        {page?.content.map((product) => (
+          <div className="col-sm-6 col-md-12">
+            <ProductCrudCard product={product} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default List;
