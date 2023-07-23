1fe08b996f54fc67b18e2cbd603b815c6a673be8
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2023-07-23 15:15:53 -0300

    Configurando rotas do CRUD

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -28,8 +28,8 @@ const Form = () => {
       data,
       withCredentials: true,
     };
-    requestBackend(config).then((response) => {
-      console.log(response.data);
+    requestBackend(config).then(() => {
+      history.push('/admin/products');
     });
   };
 
--- frontweb/src/pages/Admin/Products/ProductCrudCard/index.tsx
+++ frontweb/src/pages/Admin/Products/ProductCrudCard/index.tsx
@@ -2,39 +2,41 @@ import './styles.css';
 import ProductPrice from 'components/ProductPrice';
 import { Product } from 'types/product';
 import CategoryBadge from '../CategoryBadge';
+import { Link } from 'react-router-dom';
 
 type Props = {
-    product: Product;
-}
+  product: Product;
+};
 
-const ProductCrudCard = ({product} : Props) => {
-
-    return (
-        <div className="base-card product-crud-card">
-            <div className="product-crud-card-top-container">
-                <img src={product.imgUrl} alt={product.name}/>
-            </div>
-            <div className="product-crud-card-description">
-              <div className="product-crud-card-bottom-container">
-                <h6>{product.name}</h6>
-                <ProductPrice price={product.price}/>
-              </div>
-              <div className="product-crud-categories-container">
-                {product.categories.map((category) => (
-                    <CategoryBadge name={category.name} key={category.id}/>
-                ))}
-              </div>
-            </div>
-            <div className="product-crud-card-buttons-container">
-              <button className="btn btn-outline-danger product-crud-card-button product-crud-card-button-first">
-                EXCLUIR
-              </button>
-              <button className="btn btn-outline-secondary product-crud-card-button">
-                EDITAR
-              </button>
-            </div>           
+const ProductCrudCard = ({ product }: Props) => {
+  return (
+    <div className="base-card product-crud-card">
+      <div className="product-crud-card-top-container">
+        <img src={product.imgUrl} alt={product.name} />
+      </div>
+      <div className="product-crud-card-description">
+        <div className="product-crud-card-bottom-container">
+          <h6>{product.name}</h6>
+          <ProductPrice price={product.price} />
         </div>
-    );
-}
+        <div className="product-crud-categories-container">
+          {product.categories.map((category) => (
+            <CategoryBadge name={category.name} key={category.id} />
+          ))}
+        </div>
+      </div>
+      <div className="product-crud-card-buttons-container">
+        <button className="btn btn-outline-danger product-crud-card-button product-crud-card-button-first">
+          EXCLUIR
+        </button>
+        <Link to={`/admin/products/${product.id}`}>
+          <button className="btn btn-outline-secondary product-crud-card-button">
+            EDITAR
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default ProductCrudCard;
