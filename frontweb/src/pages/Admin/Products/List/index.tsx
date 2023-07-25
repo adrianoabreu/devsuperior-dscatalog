930d1019583f2f74b4e0d9cc8384b743777bc076
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2023-07-25 18:23:39 -0300

    Criando um estado específico para os dados dos controles

--- frontweb/src/pages/Admin/Products/List/index.tsx
+++ frontweb/src/pages/Admin/Products/List/index.tsx
@@ -8,19 +8,29 @@ import './styles.css';
 import { requestBackend } from 'util/requests';
 import Pagination from 'components/Pagination';
 
+type ControlComponentsData = {
+  activePage: number;
+}
+
 const List = () => {
   const [page, setPage] = useState<SpringPage<Product>>();
 
-  useEffect(() => {
-    getProducts(0);
-  }, []);
+  const [controlComponentsData, setControlComponentsData] = useState<ControlComponentsData>(
+    {
+      activePage: 0
+    }
+  );
 
-  const getProducts = (pageNumber: number) => {
+  const handlePageChange = (pageNumber: number) => {
+    setControlComponentsData({activePage: pageNumber});
+   };
+
+  useEffect(() => {
     const config: AxiosRequestConfig = {
       method: 'GET',
       url: '/products',
       params: {
-        page: pageNumber,
+        page: controlComponentsData.activePage,
         size: 3
       },
     };
@@ -28,7 +38,7 @@ const List = () => {
     requestBackend(config).then((response) => {
       setPage(response.data);
     });
-  };
+  }, [controlComponentsData]);
 
   return (
     <div className="product-crud-container">
@@ -44,14 +54,14 @@ const List = () => {
         {page?.content.map((product) => (
           <div className="col-sm-6 col-md-12">
             <ProductCrudCard product={product} 
-            onDelete={() => getProducts(page.number)}/>
+            onDelete={() => {}}/>
           </div>
         ))}
       </div>
       <Pagination 
         pageCount={(page) ? page.totalPages : 0}
         range={3}
-        onChange={getProducts}
+        onChange={handlePageChange}
       />
     </div>
   );
