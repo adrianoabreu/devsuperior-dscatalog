58c56d46dd5638502980ed0bc004f0bda703a0f9
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2023-07-26 17:01:28 -0300

    Criando o componente ProductFilter

--- frontweb/src/pages/Admin/Products/List/index.tsx
+++ frontweb/src/pages/Admin/Products/List/index.tsx
@@ -7,6 +7,7 @@ import { AxiosRequestConfig } from 'axios';
 import './styles.css';
 import { requestBackend } from 'util/requests';
 import Pagination from 'components/Pagination';
+import ProductFilter from 'components/ProductFilter';
 
 type ControlComponentsData = {
   activePage: number;
@@ -52,7 +53,7 @@ const List = () => {
             ADICIONAR
           </button>
         </Link>
-        <div className="base-card product-filter-container">Search bar</div>
+        <ProductFilter/>
       </div>
       <div className="row">
         {page?.content.map((product) => (
