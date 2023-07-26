abafa0d96d643970bc4d9d72463a8e41ec4b3c87
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2023-07-26 20:22:19 -0300

    Layout mobile do ProductFilter

--- frontweb/src/components/ProductFilter/index.tsx
+++ frontweb/src/components/ProductFilter/index.tsx
@@ -39,7 +39,7 @@ const ProductFilter = () => {
             placeholder="Nome do Produto"
             name="name"
           />
-          <button>
+          <button className="product-filter-search-icon">
             <SearchIcon />
           </button>
         </div>
@@ -53,15 +53,16 @@ const ProductFilter = () => {
                 <Select
                   {...field}
                   options={selectCategories}
-                  classNamePrefix="product-crud-select"
+                  classNamePrefix="product-filter-select"
                   isClearable
+                  placeholder="Categoria"
                   getOptionLabel={(category: Category) => category.name}
                   getOptionValue={(category: Category) => String(category.id)}
                 />
               )}
             />
           </div>
-          <button className="btn btn-outline-secondary">LIMPAR</button>
+          <button className="btn btn-outline-secondary btn-product-filter-clear">LIMPAR <span className="btn-product-filter-word">FILTRO</span></button>
         </div>
       </form>
     </div>
